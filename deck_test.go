6584eabd2d5e3619e48aad9023f44a978d8eb49e
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2023-03-10 19:58:45 -0800

    add TestSaveToDeckAndNewDeckFromFile test

--- deck_test.go
+++ deck_test.go
@@ -1,13 +1,25 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
-func TestNewDeck54(t *testing.T) {
+func TestNewDeckWithJokers(t *testing.T) {
 	d := newDeck(true)
 	if len(d) != 54 {
 		t.Error("wrong amount of cards")
 	}
-
+	jokerCounter := 0
+	for i := 0; i < len(d); i++ {
+		if d[i] == "Joker" {
+			jokerCounter++
+		}
+	}
+	if jokerCounter < 2 {
+		fmt.Println("Less than 2 Jokers")
+	}
 }
 
 func TestNewDeck52(t *testing.T) {
@@ -15,4 +27,12 @@ func TestNewDeck52(t *testing.T) {
 	if len(d) != 52 {
 		t.Error("wrong amount of cards")
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck(true)
+	d.saveToFile("_decktesting")
+	deckFromFile := newDeckFromFile("_decktesting")
+	fmt.Println(len(deckFromFile) == 54)
 }
